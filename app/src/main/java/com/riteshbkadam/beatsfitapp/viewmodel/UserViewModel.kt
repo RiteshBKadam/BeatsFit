f25559a14f9d3ba1b8cbb0497e28903af26c2032
Hyrider <kadamrb04@gmail.com>
2025-03-31 03:01:00 +0530

    .

---
+++ app/src/main/java/com/riteshbkadam/beatsfitapp/viewmodel/UserViewModel.kt
@@ -0,0 +1,34 @@
+package com.riteshbkadam.beatsfitapp.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.riteshbkadam.beatsfitapp.room.data.User
+import com.riteshbkadam.beatsfitapp.room.data.UserRepository
+import kotlinx.coroutines.launch
+
+
+class UserViewMode(private val repository: UserRepository) : ViewModel() {
+
+    private val _user = MutableLiveData<User?>()
+    val user: LiveData<User?> get() = _user
+
+    init {
+        fetchUser()
+    }
+
+    private fun fetchUser() = viewModelScope.launch {
+        _user.postValue(repository.getUser())
+    }
+
+    fun clearDatabase() = viewModelScope.launch {
+        repository.clearDatabase()
+    }
+
+
+    fun insertUser(user: User) = viewModelScope.launch {
+        repository.insertUser(user)
+        fetchUser() // Refresh user data after insert
+    }
+}
