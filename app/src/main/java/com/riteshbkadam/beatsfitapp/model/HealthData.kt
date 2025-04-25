ec8dd04d896c10d48b261145b6747147971ded93
Hyrider <kadamrb04@gmail.com>
2025-04-26 03:33:51 +0530

    updated

---
+++ app/src/main/java/com/riteshbkadam/beatsfitapp/model/HealthData.kt
@@ -0,0 +1,11 @@
+package com.riteshbkadam.beatsfitapp.model
+
+data class HealthData(
+    val steps: Float,
+    val distance: Float,
+    val heartRate: Float,
+    val calories: Float,
+    val latitude: Double,
+    val longitude: Double,
+    val sleepData: Map<String, String>
+)
