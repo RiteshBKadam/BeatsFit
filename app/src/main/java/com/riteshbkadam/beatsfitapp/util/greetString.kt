e899d3af7285717b3a5e1ae027ff1d5b05843e9c
Hyrider <kadamrb04@gmail.com>
2025-04-27 03:34:39 +0530

    updated

---
+++ app/src/main/java/com/riteshbkadam/beatsfitapp/util/greetString.kt
@@ -0,0 +1,14 @@
+package com.riteshbkadam.beatsfitapp.util
+
+import java.util.Calendar
+
+fun greetString(): String {
+    val calendar = Calendar.getInstance()
+    val hour = calendar.get(Calendar.HOUR_OF_DAY)
+
+    return when (hour) {
+        in 6..11 -> "Morning"
+        in 12..16 -> "Afternoon"
+        else ->"Evening"
+    }
+}
