00be96e012d8cac85551f26915a73ecfc03e386e
Hyrider <kadamrb04@gmail.com>
2025-04-29 03:35:30 +0530

    updated

---
+++ app/src/main/java/com/riteshbkadam/beatsfitapp/util/BootReciever.kt
@@ -0,0 +1,57 @@
+package com.riteshbkadam.beatsfitapp.util
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import com.riteshbkadam.beatsfitapp.R
+import com.riteshbkadam.beatsfitapp.view.MainActivity
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent?) {
+        if (intent?.action == Intent.ACTION_BOOT_COMPLETED) {
+            showStartLocationNotification(context)
+        }
+    }
+
+    private fun showStartLocationNotification(context: Context) {
+        val channelId = "boot_channel"
+        val channelName = "Boot Notification Channel"
+        val notificationId = 101
+
+        val notificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val intent = Intent(context, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(
+            context,
+            0,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val notification = NotificationCompat.Builder(context, channelId)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("BeatsFit is Ready")
+            .setContentText("Tap to start location tracking.")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .build()
+
+        notificationManager.notify(notificationId, notification)
+    }
+}
